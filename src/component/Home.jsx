50683d5bc50e6be9e15e39cea57228f151f0513c
Roushan <roushansingh3698@gmail.com>
2024-06-29 13:54:19 +0530

    home page scroll animation added

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,6 +5,11 @@ import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom'
 import './Home.scss'
 import './Contact'
+import { ScrollTrigger } from "gsap/all";
+import { gsap } from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+
 
 import './HomeP.scss'
 import apple from './image/AppleStore.webp'
@@ -21,15 +26,30 @@ import img9 from './image/9th_img.webp'
 import img11 from './image/11th_BG.webp'
 import Contact from './Contact';
 
+gsap.registerPlugin(ScrollTrigger)
+
 const appLink ="https://www.apple.com/in/?afid=p238%7Csfo291LJq-dc_mtid_187079nc38483_pcrid_680861337497_pgrid_109516736059_pntwk_g_pchan__pexid__&cid=aos-IN-kwgo-brand--slid---product-"
 
 const GLink = 'https://play.google.com/store/games?device=windows&utm_source=apac_med&hl=en-IN&utm_medium=hasem&utm_content=Jun1223&utm_campaign=Evergreen&pcampaignid=MKT-EDR-apac-in-1003227-med-hasem-py-Evergreen-Jun1223-Text_Search_BKWS-BKWS%7CONSEM_kwid_43700076783011871_creativeid_662052581180_device_c&gclid=Cj0KCQiAkKqsBhC3ARIsAEEjuJhkqNnZFUWaMljPH81yMMTCAHIsbdergeJL0UF8nJihszG1vAn-OSIaAsbaEALw_wcB&gclsrc=aw.ds&pli=1'
 
 
 const Home = () => {
-  // const {ref,inView} =useInView({
-  //   threshold:0.5,
-  // })
+  // // TODO: Implement the gsap scroll trigger
+  useGSAP(() => {
+      gsap.to('.scrollBox', {
+        x: 120,
+        scale: 1,
+        scrollTrigger: {
+          trigger: '.scrollBox',
+          start: 'top top',
+          end: 'bottom 20%',
+          scrub: 5,
+        },
+        ease: 'power1.inOut'
+      })
+    
+  },[])
+
   const [div0Ref, div0InView] = useInView({
     triggerOnce: true,
     rootMargin: '0px',
@@ -106,49 +126,7 @@ const Home = () => {
     threshold: 0.4
   });
 
-  // const [inputValue, setInputValue] = useState('');
-  // const [inputValue1, setInputValue1] = useState('');
-  // const [inputValue2, setInputValue2] = useState('');
-  // const [inputValue3, setInputValue3] = useState('');
-  // const [inputValue4, setInputValue4] = useState('');
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   // Do something with the submitted data
-  //   console.log('Submitted:', inputValue);
-
-  //   // Reset the input field
-  //   setInputValue('');
-  //   setInputValue1('');
-  //   setInputValue2('');
-  //   setInputValue3('');
-
-  // };
-  // const handleSubmit1 = (e) => {
-  //   e.preventDefault();
-  //   // Do something with the submitted data
-  //   console.log('Submitted:', inputValue);
-
-  //   // Reset the input field
-  //   setInputValue4('');
-
-  // };
-
-  // const handleInputChange = (e) => {
-  //   setInputValue(e.target.value);
-  // };
-  // const handleInputChange1 = (e) => {
-  //   setInputValue1(e.target.value);
-  // };
-  // const handleInputChange2 = (e) => {
-  //   setInputValue2(e.target.value);
-  // };
-  // const handleInputChange3 = (e) => {
-  //   setInputValue3(e.target.value);
-  // };
-  // const handleInputChange4 = (e) => {
-  //   setInputValue4(e.target.value);
-  // };
 
 
   function ourgame(){
@@ -159,7 +137,8 @@ const Home = () => {
   return (
     <>
 
-      <section className='section1'>
+      <section className='section1' >
+          <div className='scrollBox'></div>
         <div className='firstSection'>
           <h1 className='head'>Made to Be Played </h1>
           <p>The Very Best in Mobile Gaming</p>
@@ -168,6 +147,7 @@ const Home = () => {
             <Link to={GLink} target="_blank"><img className='AppGoogle' src={Google} alt="" /></Link>
           </div>
         </div>
+        
       </section>
 
       <section className='section2'>
@@ -319,50 +299,6 @@ const Home = () => {
         </div>
         <div className="fixedbg2">
           <Contact/>
-          {/* <div className="r3">
-            <div className="formC formC0" id='Contact'>
-              <h2>Get in Touch</h2>
-              <p className="paragraph">I'm a paragraph. Click here to add your own text and edit me.</p>
-              <form onSubmit={handleSubmit} >
-                <div className='namediv widthHeight'>
-                  <div>
-                    <label className='paragraph' for="fname">First Name *</label> <br />
-                    <input type="text" name="Fname" onChange={handleInputChange} value={inputValue} id="fname" className='Input In1' />
-                  </div>
-                  <div>
-                    <label className='paragraph' for="lname">Last Name *</label> <br />
-                    <input type="text" name="Lname" onChange={handleInputChange1} value={inputValue1} id="lname" className='Input In1' />
-                  </div>
-
-                </div>
-                <div className='mailDiv widthHeight'>
-                  <label className='paragraph' for="email">Email *</label> <br />
-                  <input type="email" name="Email" onChange={handleInputChange2} value={inputValue2} id="email" className='Input In2' />
-                </div>
-
-                <div className='mailDiv widthHeight message'>
-                  <label className='paragraph' for="comment">Leave us a message...</label> <br />
-                  <textarea name="myText" onChange={handleInputChange3} value={inputValue3} id="comment" cols="30" rows="7"></textarea>
-                </div>
-
-                <button type="submit">Submit</button>
-              </form>
-            </div>
-          </div>
-          <div className="r3">
-            <div className="formC formC1 width">
-              <h3 className='redP Mar r3h3'>Subscribe to Our Newsletter</h3>
-              <form onSubmit={handleSubmit1}>
-                <div >
-                  <div className='mailDiv widthHeight subsDiv'>
-                    <label className='paragraph' for="email">Email *</label>
-                    <input type="email" name="Email" onChange={handleInputChange4} value={inputValue4} id="email" className='Input subscribeInput In1' />
-                  </div>
-                  <button className='BBton' type="submit">Join</button>
-                </div>
-              </form>
-            </div>
-          </div> */}
         </div>
       </section>
     </>
